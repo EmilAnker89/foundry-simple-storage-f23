a33834fcd0faafc10d773e70b5568a597e500ead
Emil Anker Jørgensen <emjo@bitcoinsuisse.com>
2023-11-26 19:46:52 +0100

    .env.example update

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-
+// part 1, time: 7:29
 pragma solidity 0.8.19;
 
+//import {Script} from "../lib/forge-std/src/Script.sol";
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
